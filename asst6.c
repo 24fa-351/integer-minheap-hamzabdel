b55c44fcbd77ff8de82b79f3f251993051ee003a
hamzabdel <hamzaabdel@csu.fullerton.edu>
2024-10-03 00:28:52 -0700

    program runs

--- asst6.c
+++ asst6.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdint.h>
 
-#include "heap.h"
+#include "some_heap.h"
 
 /*
 Assignment 5: integer minheap
@@ -18,16 +19,21 @@ void test_heap(void) {
     heap_t *heap = heap_create(200);
     for (heap_key_t ix = 0; ix < 20; ix++) {
         heap_key_t key = rand_between(0, 1000);
-        heap_insert(heap, key, (heap_value_t)key);
+        heap_insert(heap, key, (heap_value_t)(uintptr_t)key);
         heap_print(heap);
     }
     for (int ix = 0; ix < 10; ix++) {
-        heap_key_t key = (heap_key_t)heap_remove_min(heap);
-        printf("Removed %llu\n", key);
+        heap_value_t removed_value = heap_remove_min(heap);
+        if (removed_value != NULL) {
+            printf("Removed %llu\n", (unsigned long long)(uintptr_t)removed_value);
+        } else {
+            printf("Heap is empty!\n");
+        }
         heap_print(heap);
     }
-    exit(0);
+    heap_free(heap);
 }
+
 int main(int argc, char *argv[]) {
     srand(time(NULL));
 
